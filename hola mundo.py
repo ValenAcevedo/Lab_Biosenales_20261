2d3c8eb32cb966300c1896ff5634df628a8d9073
ValenAcevedo <valentina.acevedo3@udea.edu.co>
2026-02-16 19:28:48 -0500

    hola mundo

--- hola mundo.py
+++ hola mundo.py
@@ -1,5 +1,12 @@
 print("Hola mundogit")
 
-# lo que está en la carpeta agregarlo al repositorio: git add .
+# Clonar el repositorio en el PC: git clone (+ link del repositorio)
+
+# lo que está en la carpeta agregarlo al repositorio (todo lo que está en el entorno de trabajo, téngalo en cuenta): git add .
 
 # Subir un commit al github: git commit -m "nombre del archivo"
+
+# Subir los cambios al repositorio: git push
+# Subir los cambios al repositorio remoto: git push origin main
+
+# Bajar los cambios del repositorio: git pull
